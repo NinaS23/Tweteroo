85f86f53208046a33dfb91140de57459a0114828
MariaDuda <mariabevenuto062@gmail.com>
2022-04-22 12:43:17 -0300

    get e post feito falta um post

--- back-end/index.js
+++ back-end/index.js
@@ -4,37 +4,42 @@ import cors from "cors";
 const server = express();
 server.use(express.json());
 server.use(cors());
-const user =[];
-const tweet =[];
+const users =[];
+const tweet = [{
+    username: "bobesponja",
+    tweet: "eu amo o hub",
+    
+}];
 
-server.post("/sign-up" , (req,resp)=>{
-   if( req.body.username === "" || req.body.avatar === ""){
+server.post("/sign-up", (req, resp) => {
+    const user = req.body
+    if (user.username === "" || user.avatar === "") {
         resp.status(400).send("Todos os campos são obrigatórios!");
-        return;
     } else {
-       const users = req.body.user
-        user.push(users);
-        resp.status(201).send("OK")
+        users.push(user);
+        resp.status(201).json("OK")
     }
 })
 
 server.post("/tweets", (req, resp) => {
     if (req.body.username === "" || req.body.tweet === "") {
         resp.status(400).send("Todos os campos são obrigatórios!");
-        return;
+       
     } else {
         tweet.push({ tweet: req.body.tweet, username: req.body.username, avatar });
-        resp.send("OK")
+        resp.status(201).send("OK")
     }
 })
 
-/* server.get("/tweets",(req,resp)=>{
+server.get("/tweets",(req,resp)=>{
     const QtdMax = 10;
-    const pageTweteroo = req.body.page;//aqui fiz uma req do tipo bodyt pra page
-    if(pageTweteroo > QtdMax){
-        
-    }
-}) */
+    const page =[];
+   for(let i =0; i < QtdMax;i++){
+       page.push(tweet[i])
+
+   }
+   resp.status(201).send(page)
+}) 
 
 
 server.listen(5000, ()=>{
@@ -47,4 +52,6 @@ filtrando apenas os 10 ultimos tweets
 
 1- primeiro , tem que havar uma page que irá ter todos os tweets , todos (sem filtrar)
 2- depois , filtrar eles : se a page tiver mais do que 10 tweets , fazer um desctruturing de tweets e add, apenas 10 ultimos
+
+@@ arranjar um jeito de colocar o avatar(img)no obj user no get; e cospe tudo isso na resp
 */
